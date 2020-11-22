a3ea2326d702156e7b1b9b0d9c768b6a6cbfc015
Adharsh Kamath <adharshkamathr@gmail.com>
2020-11-22 21:08:10 +0530

    Log format changed

--- src/log.erl
+++ src/log.erl
@@ -7,10 +7,10 @@
 logThis(Message, FileName, VClock, Modes) ->
     {ok, Device} = file:open(FileName ++ "_log.txt", Modes),
     try 
-        writeLogs(Device, Message, VClock)
+        writeLogs(Device, Message, VClock, FileName)
         after file:close(Device)
     end,
     ok.
 
-writeLogs(S, Message, VClock) ->
-    io:format(S, "~s~n~n~w~n~n", [Message, VClock]).
+writeLogs(S, Message, VClock, Node) ->
+    io:format(S, "~s~n~s ~w~n", [Message, Node, VClock]).
